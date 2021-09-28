7f87f38cfa660d4150dfbc78b318043f14ec3f3a
anirbandas10 <anirbandas1127@gmail.com>
2021-09-28 21:14:35 +0530

    Updated

--- CodeSample.c
+++ CodeSample.c
@@ -12,18 +12,6 @@ int main() {
     case '+':
       printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
       break;
-    case '-':
-      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
-      break;
-    case '*':
-      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
-      break;
-    case '/':
-      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
-      break;
-    // operator doesn't match any case constant
-    default:
-      printf("Error! operator is not correct");
   }
 
   return 0;
